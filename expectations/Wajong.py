f1ac939639d4462fbe177b698ba9a4e5b9893b94
Bouke <bouke@iridis.nl>
2025-03-19 15:50:51 +0100

    Added docstrings.

--- expectations/Wajong.py
+++ expectations/Wajong.py
@@ -1,27 +1,19 @@
 class Wajong:
 
-    """"
-    Calculate monthly benefit. Scenario with income not supported.
-    """
     def calculate_benefit(self, age: int, can_work: bool, income = 0):
+        """ Calculate monthly benefit. Scenario with income not supported. """
         if income > 0:
             raise NotImplementedError("Calculation with income not supported.")
         if age < 18:
             raise ValueError("Age should be at least 18 years.")
-        match age:
-            case 18:
-                if can_work:
-                    return 790.40
-                return 845.20
-            case 19:
-                if can_work:
-                    return 942.79
-                return 1008.54
-            case 20:
-                if can_work:
-                    return 1240.81
-                return 1328.48
-            case _:  # > 21
-                if can_work:
-                    return 1534.22
-                return 1643.81
+
+        benefit_amounts = {
+            18: (790.40, 845.20),
+            19: (942.79, 1008.54),
+            20: (1240.81, 1328.48),
+            21: (1534.22, 1643.81)
+        }
+
+        key = age if age < 21 else 21
+
+        return benefit_amounts[key][0 if can_work else 1]
--- lib/Format.py
+++ lib/Format.py
@@ -1,4 +1,5 @@
 class Format:
 
     def format_currency(self, value: float):
+        """ Formats for example 1200,50 as 1.200,50 """
         return f"{float(value):_.2f}".replace(".", ",").replace("_", ".")

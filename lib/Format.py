b7fc68b02c1e490c2363d7fda8a0f83bc642ef6e
Bouke <bouke@iridis.nl>
2025-03-19 15:54:35 +0100

    Corrected docstring.

--- lib/Format.py
+++ lib/Format.py
@@ -1,5 +1,5 @@
 class Format:
 
     def format_currency(self, value: float):
-        """ Formats for example 1200,50 as 1.200,50 """
+        """ Formats for example 1200.5 as 1.200,50 """
         return f"{float(value):_.2f}".replace(".", ",").replace("_", ".")
